d1b92171a6f3e6abf215a68909c1f852f33535b7
cyberlakki <100266461+cyberlakki@users.noreply.github.com>
2022-06-24 11:09:39 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+        $(document).ready(function(){
+         $(".navbar a").click(function(){
+          $(".navbar a").css("background-color","")
+          $(this).css("background-color","rgba(95, 158, 160, 0.581)")
+         })
+         $(".navbar a").hover(function(){
+           $(".navbar a").removeClass("hover")
+           $(this).addClass("hover")
+         })
+         $(".footer-link ul li").hover(function(){
+           $(".footer-link ul li").removeClass("hover")
+           $(this).addClass("hover")
+         })
+        })  
